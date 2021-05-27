4607f623eb0ce4839e2ff6e82184fc7c2944a151
Tang jiyi <37148354+Tangjiyi0416@users.noreply.github.com>
2021-05-27 11:33:49 +0800

    Add files via upload

---
+++ led_dimming_with_photoresistorled_1.ino
@@ -0,0 +1,22 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(9, OUTPUT);
+  Serial.begin(9600);
+}
+int sensorValue;
+int dim;
+
+
+void loop()
+{
+  sensorValue = analogRead(A0);
+  Serial.print("sensorValue = ");
+  Serial.print(sensorValue);
+  dim = map(sensorValue,750,1000,0,255);
+  if(dim<0)dim=0;
+  Serial.print(", dim = ");
+  Serial.println(dim);
+  analogWrite(9, dim);
+}
